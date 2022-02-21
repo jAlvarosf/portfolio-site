90c368db0b27c4ae3c5fc719049e0eedaeb421b0
José Alvaro <jose.alvaro.sf@hotmail.com>
2022-02-21 11:18:48 -0300

    atualização na sessão sobre

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     //toggle menu/navbar script
     $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
+        // $('.menu-btn i').toggleClass("active");
     });
 });
 
